0db62e31fc1322297e2a98601284834123c8a3ae
yusuke0621 <yusuke19930621@gmail.com>
2024-12-19 23:47:44 +0900

    feature/blog-next-graphql

--- app/page.js
+++ app/page.js
@@ -1,101 +1,61 @@
 import Image from "next/image";
+import { GraphQLClient, gql } from "graphql-request";
+import styles from "../styles/Home.modules.css"; // 必要に応じてスタイルファイルを追加
+import BlogCard from "../components/BlogCard";
 
-export default function Home() {
+const graphcms = new GraphQLClient(
+  "https://us-west-2.cdn.hygraph.com/content/cm4u94wj700jz07vy8v216bmc/master"
+);
+
+const QUERY = gql`
+  {
+    posts {
+      id
+      title
+      datePublish
+      slug
+      content
+    }
+    author(where: { id: "cm4vcamznrjh907n4eymqykvd" }) {
+      name
+      avater {
+        url
+      }
+    }
+  }
+`;
+// class="grid grid-cols-2 gap-[90px]" graphpaperのスタイル
+export default async function Home() {
+  // データを取得
+  const data = await graphcms.request(QUERY);
+  const { posts, author } = data;
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div class="text-[1.8rem]">
+          <h2>Next.jsとGraphQLブログ</h2>
         </div>
+        {/* 投稿リストのレンダリング */}
+        <div>
+          {posts.map((post) => (
+            //
+            <BlogCard />
+          ))}
+        </div>
+        {/* 著者情報のレンダリング */}
+        {/* <div className="author">
+          <h4>Author: {author.name}</h4>
+          {author.avater?.url && (
+            <Image
+              src={author.avater.url}
+              alt={author.name}
+              width={100}
+              height={100}
+            />
+          )}
+        </div> */}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
